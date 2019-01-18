f3acced418933f436c2f8cf787c5c39189093b42
Elena741 <42449326+Elena741@users.noreply.github.com>
2019-01-18 18:09:08 +0300

    Add files via upload

---
+++ app/Category.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Category
+ *
+ * @package App
+ */
+class Category extends Model
+{
+    // Массовое заполнение
+    protected $fillable = [
+    	'title', 
+    	'published'
+    ];
+
+    // Связь с таблицей questions один ко многим
+    
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function questions() {
+    	
+            return $this->hasMany('App\Question');
+
+        }
+
+    // Возвращает все опубликованные категории
+    
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    public static function published()
+    {
+        return Category::where('published', 1)->get();
+    }
+    
+    // Возвращает все опубликованные категории имеющие хотя бы один вопрос
+    
+    /**
+     * @return \App\Category[]|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Support\Collection
+     */
+    public static function related()
+    {
+        return Category::has('questions')->where('published', 1)->get();
+    }
+}
